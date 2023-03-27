b964003c9436a1662a0d264a0ae9673d565cf991
Dois <DoisKoh@users.noreply.github.com>
2023-03-27 16:19:55 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spawn Times
+
+A website showing when Diablo IV world bosses/events happen. Subject to Diablo IV API's becoming publically available.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<meta name="description" content="Diablo IV world boss and event spawn timers." />
+	<meta name="keywords" content="diablo, diablo 4, diablo iv, world boss, event, spawn time" />
+	<title>Diablo IV Spawn Times</title>
+</svelte:head>
+<div class="container text-center h-full text-6xl flex justify-center items-center">
+	Coming Soon (June 2023)
+</div>
